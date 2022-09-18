06de83a4d802328ba07df4eed88190c53befaabf
norohind <60548839+norohind@users.noreply.github.com>
2022-09-18 14:43:21 +0300

    Fully done migration to demb.uk base domain

--- EliteApiBot/Infrastructure/Squad/SquadRequester.cs
+++ EliteApiBot/Infrastructure/Squad/SquadRequester.cs
@@ -13,10 +13,9 @@ public class SquadRequester
     public async Task<string?> Request(string tag,  bool isFull = false)
     {
         var url = isFull 
-            ? $"https://146.66.200.10/api/squads/now/by-tag/extended/{tag}?resolve_tags=true&pretty_keys=true"
-            : $"https://146.66.200.10/api/squads/now/by-tag/extended/{tag}?pretty_keys=true";
+            ? $"https://sapi.demb.uk/api/squads/now/by-tag/extended/{tag}?resolve_tags=true&pretty_keys=true"
+            : $"https://sapi.demb.uk/api/squads/now/by-tag/extended/{tag}?pretty_keys=true";
         using var request = new HttpRequestMessage(HttpMethod.Get, url);
-        request.Headers.Host = "sapi.demb.uk";
         using var httpResponse = await client.SendAsync(request);
 
         if (!httpResponse.IsSuccessStatusCode)
--- EliteApiBot/Utils/Constants.cs
+++ EliteApiBot/Utils/Constants.cs
@@ -7,9 +7,9 @@ namespace EliteApiBot.Utils;
 public static class Constants
 {
     public const string DateTimeFormat = "dd/MM/yyyy HH:mm:ss";
-    public const string ShortLink = "https://146.66.200.10/squads/now/by-tag/short/{0}";
-    public const string ExtendedLink = "https://146.66.200.10/squads/now/by-tag/extended/{0}?resolve_tags=true";
-    public const string CsvLink = "https://146.66.200.10/a31/CEC-list-monitoring/raw/branch/master/list.csv";
+    public const string ShortLink = "https://sapi.demb.uk/squads/now/by-tag/short/{0}";
+    public const string ExtendedLink = "https://sapi.demb.uk/squads/now/by-tag/extended/{0}?resolve_tags=true";
+    public const string CsvLink = "https://sapi.demb.uk/a31/CEC-list-monitoring/raw/branch/master/list.csv";
 
     public static readonly JsonSerializerSettings JsonSerializerSettings = new()
     {
